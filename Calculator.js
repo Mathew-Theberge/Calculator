d13fa0aec45e6845d174daee8a1fd35680f28634
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-07-30 19:58:13 -0400

    Add DOM references for all buttons

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,23 @@
+const clearButton = document.querySelector(".clear-button")
+const negativeNumbersButton = document.querySelector(".negative-numbers-button")
+const exponentButton = document.querySelector(".exponent-button")
+const dotButton = document.querySelector(".dot-button")
+const equalsButton = document.querySelector(".equals-button")
+const n1 = document.querySelector(".n1")
+const n2 = document.querySelector(".n2")
+const n3 = document.querySelector(".n3")
+const n4 = document.querySelector(".n4")
+const n5 = document.querySelector(".n5")
+const n6 = document.querySelector(".n6")
+const n7 = document.querySelector(".n7")
+const n8 = document.querySelector(".n8")
+const n9 = document.querySelector(".n9")
+const n0 = document.querySelector(".n0")
+const divideButton = document.querySelector(".divide-button")
+const multiplyButton = document.querySelector(".multiply-button")
+const subtractButton = document.querySelector(".subtract-button")
+const addButton = document.querySelector(".add-button")
+
 function add(num1, num2) {
   return num1 + num2
 }
@@ -33,6 +53,3 @@ function operate(num1, num2, operator) {
       divide(num1, num2)
   }
 }
-
-
-operate(operand1, operand2, operator)
