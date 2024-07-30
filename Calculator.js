75db29a5314fcaa2a5fcd8d765d0f1b57323ed0c
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-07-30 19:32:43 -0400

    Add operate function

--- Calculator.js
+++ Calculator.js
@@ -3,14 +3,36 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-  return num1 - num2
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return num1 * num2
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return num1 / num2
+  return num1 / num2;
 }
-console.log(subtract(9, 3))
+
+let operand1 = 2
+let operand2 = 3
+let operator = "add"
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "add":
+      add(num1, num2)
+      break
+    case "subtract":
+      subtract(num1, num2)
+      break
+    case "multiply":
+      multiply(num1, num2)
+      break
+    case "divide":
+      divide(num1, num2)
+  }
+}
+
+
+operate(operand1, operand2, operator)
