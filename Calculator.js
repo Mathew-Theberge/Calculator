21336574dd1b4b374ddd25c0841a2003433583c4
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-08-02 15:05:20 -0400

    Remove old button login and create updatedisplayvalue function
    the old button logic did not allow for the switching of
    operators i.e 2 + 2 - 1 + 3 would not work as intended
    i beleive rewriting the logic is neccesary. also
    added updatedisplayvalue function for updateing the display
    when number buttons are hit instead of using arrow functions
    for each button should help simplify the code and make
    changes that may need to happen eaiser

--- Calculator.js
+++ Calculator.js
@@ -20,223 +20,51 @@ const addButton = document.querySelector(".add-button")
 const displayOutput = document.querySelector(".display-output")
 
 function add(num1, num2) {
-  if (Math.floor(num1 + num2) === num1 + num2) {
-    displayOutput.textContent = num1 + num2
-    displayOutput.classList.add("bold")
-  } else {
-    displayOutput.textContent = (num1 + num2).toFixed(7)
-    displayOutput.classList.add("bold")
-  }
+
 }
 
 function subtract(num1, num2) {
-  if (Math.floor(num1 - num2) === num1 - num2) {
-    displayOutput.textContent = num1 - num2
-    displayOutput.classList.add("bold")
-  } else {
-    displayOutput.textContent = (num1 - num2).toFixed(7)
-    displayOutput.classList.add("bold")
-  }
+
 }
 
 function multiply(num1, num2) {
-  if (Math.floor(num1 * num2) === num1 * num2) {
-    displayOutput.textContent = num1 * num2
-    displayOutput.classList.add("bold")
-  } else {
-    displayOutput.textContent = (num1 * num2).toFixed(7)
-    displayOutput.classList.add("bold")
-  }
+
 }
 
 function divide(num1, num2) {
-  if (Math.floor(num1 / num2) === num1 / num2) {
-    displayOutput.textContent = num1 / num2
-    displayOutput.classList.add("bold")
-  } else {
-    displayOutput.textContent = (num1 / num2).toFixed(7)
-    displayOutput.classList.add("bold")
-  }
+
 }
 
 function exponent(num1, num2) {
-  if (Math.floor(num1 ** num2) === num1 ** num2) {
-    displayOutput.textContent = num1 ** num2
-    displayOutput.classList.add("bold")
-  } else {
-    displayOutput.textContent = (num1 ** num2).toFixed(7)
-    displayOutput.classList.add("bold")
-  }
+
 }
 
 let displayValue = ""
 let operand1 = ""
 let operand2 = ""
 let operator = ""
+let result = ""
 
 function operate(num1, num2, operator) {
-  switch (operator) {
-    case "add":
-      add(num1, num2)
-      break
-    case "subtract":
-      subtract(num1, num2)
-      break
-    case "multiply":
-      multiply(num1, num2)
-      break
-    case "divide":
-      divide(num1, num2)
-      break
-    case "exponent":
-      exponent(num1, num2)
-  }
+
 }
 
-n1.addEventListener("click", () => {displayOutput.textContent = displayValue += "1";displayOutput.classList.remove("bold")})
-n2.addEventListener("click", () => {displayOutput.textContent = displayValue += "2";displayOutput.classList.remove("bold")})
-n3.addEventListener("click", () => {displayOutput.textContent = displayValue += "3";displayOutput.classList.remove("bold")})
-n4.addEventListener("click", () => {displayOutput.textContent = displayValue += "4";displayOutput.classList.remove("bold")})
-n5.addEventListener("click", () => {displayOutput.textContent = displayValue += "5";displayOutput.classList.remove("bold")})
-n6.addEventListener("click", () => {displayOutput.textContent = displayValue += "6";displayOutput.classList.remove("bold")})
-n7.addEventListener("click", () => {displayOutput.textContent = displayValue += "7";displayOutput.classList.remove("bold")})
-n8.addEventListener("click", () => {displayOutput.textContent = displayValue += "8";displayOutput.classList.remove("bold")})
-n9.addEventListener("click", () => {displayOutput.textContent = displayValue += "9";displayOutput.classList.remove("bold")})
-n0.addEventListener("click", () => {displayOutput.textContent = displayValue += "0";displayOutput.classList.remove("bold")})
+function updateDisplayOutput (num) {
+  displayValue += num
+  displayOutput.textContent = displayValue
+  displayOutput.classList.remove("bold")
+}
+
+n1.addEventListener("click",updateDisplayOutput.bind(null, "1"))
+n2.addEventListener("click",updateDisplayOutput.bind(null, "2"))
+n3.addEventListener("click",updateDisplayOutput.bind(null, "3"))
+n4.addEventListener("click",updateDisplayOutput.bind(null, "4"))
+n5.addEventListener("click",updateDisplayOutput.bind(null, "5"))
+n6.addEventListener("click",updateDisplayOutput.bind(null, "6"))
+n7.addEventListener("click",updateDisplayOutput.bind(null, "7"))
+n8.addEventListener("click",updateDisplayOutput.bind(null, "8"))
+n9.addEventListener("click",updateDisplayOutput.bind(null, "9"))
+n0.addEventListener("click",updateDisplayOutput.bind(null, "0"))
 
 
-addButton.addEventListener("click", () => {
-  operator = "add"
-  if (operand1 === "") {
-    operand1 = +displayValue
-    displayValue = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  } else {
-    operand1 = operand1 + operand2
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  }
-})
 
-subtractButton.addEventListener("click", () => {
-  operator = "subtract"
-  if (operand1 === "") {
-    operand1 = +displayValue
-    displayValue = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  } else {
-    operand1 = operand1 - operand2
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  }
-})
-
-multiplyButton.addEventListener("click", () => {
-  operator = "multiply"
-  if (operand1 === "") {
-    operand1 = +displayValue
-    displayValue = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  } else {
-    operand1 = operand1 * operand2
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  }
-})
-
-divideButton.addEventListener("click", () => {
-  operator = "divide"
-  if (operand1 === "") {
-    operand1 = +displayValue
-    displayValue = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  } else {
-    operand1 = operand1 / operand2
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  }
-})
-
-exponentButton.addEventListener("click", () => {
-  operator = "exponent"
-  if (operand1 === "") {
-    operand1 = +displayValue
-    displayValue = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  } else {
-    operand1 = operand1 ** operand2
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-  }
-})
-
-equalsButton.addEventListener("click", () => {
-// the reason for the first if statement is solely to remove the bold class from the output number if equals is pressed when no value is set to operand1 this code is needed to make
-// the first output bold as in 6 = outputs a bold 6 when = is pressed again it removes the bold and sets a 0
-// i have to run the opertation to keep the number zero and not an empty string
-  if (operand1 === "") {
-    operand1 = +displayValue
-    operate(operand1, operand2, "add")
-    if (operand1 === 0) {
-      displayOutput.classList.remove("bold")
-    }
-    displayValue = ""
-    operand1 = ""
-    operand2 = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-    operand1 = ""
-    operand2 = ""
-  } else if (operand2 !== "") {
-    switch (operator) {
-      case "add":
-        operand1 = operand1 + operand2
-        break
-      case "subtract":
-        operand1 = operand1 - operand2
-        break
-      case "multiply":
-        operand1 = operand1 * operand2
-        break
-      case "divide":
-        operand1 = operand1 / operand2
-        break
-      case "exponent":
-        operand1 = operand1 ** operand2
-    }
-    operand2 = +displayValue
-    operate(operand1, operand2, operator)
-    displayValue = ""
-    operand1 = ""
-    operand2 = ""
-  }
-})
-
-clearButton.addEventListener("click", () => {
-  displayOutput.textContent = 0
-  displayValue = ""
-  operand1 = ""
-  operand2 = ""
-})
