366fb888955acdd60b802cda70ff5a0d1d7423b4
Mathew Theberge <140298808+Mathew-Theberge@users.noreply.github.com>
2024-08-02 18:24:07 -0400

    Add safeguards to exponent button

--- Calculator.js
+++ Calculator.js
@@ -114,22 +114,33 @@ function divide() {
 }
 
 function exponent() {
-  if (operand1 === "") {
-    operand1 = +displayValue
-    displayValue = ""
-  } else if (operand2 === "") {
-    operand2 = +displayValue
-    displayValue = ""
-    result = operand1 ** operand2
-    displayOutput.textContent = result
-  } else {
-    operand2 = +displayValue
-    displayValue = ""
-    if (!(operand2 === 0)) {
-      result = result ** operand2
-      displayOutput.textContent = result
-    }
-  }
+  if (!(displayValue === "")) {
+    if (operand1 === "") {
+      operand1 = +displayValue
+      displayValue = ""
+    } else if (operand2 === "") {
+      operand2 = +displayValue
+      displayValue = ""
+      if (operand2 === 0 || operand1 === 0) { 
+        operand1 = ""
+        operand2 = ""
+        displayOutput.textContent = 0
+      } else {
+        result = operand1 ** operand2
+        displayOutput.textContent = result
+      }
+    } else {
+      operand2 = +displayValue
+      displayValue = ""
+      if (operand2 === 0) {
+        operand1 = ""
+        operand2 = ""
+        displayOutput.textContent = 0
+      } else {
+        result = result ** operand2
+        displayOutput.textContent = result
+      }
+  }}
 }
 
 let displayValue = ""
